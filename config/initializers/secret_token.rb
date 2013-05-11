bd83e285627eb87b7172edc9b4b17634ecac5bed
tchen <tchen@TChens-MacBook-2.local>
2013-05-10 22:13:37 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootcampApp::Application.config.secret_token = 'd2ac303e675d5c17e7a8cf3543635455ee735648c2ea75c81ac42a35448c4e48bee529270f26be5e524baa30e5c9a26a18ec2e04c33f287e5703823caa23f6e5'
