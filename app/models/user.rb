fb24cadfaff96594216bac1f0414a5b601c18096
tchen <tchen@TChens-MacBook-2.local>
2013-05-11 16:22:10 -0500

    Added tasks to show view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :assignments
   has_many :projects, :through => :assignments
+  has_many :tasks
 
   validates :email, uniqueness: true
   validates :email, :name, :password, :presence => true
